30101793421598f428b449fa5d381313c567edba
facu98 <facuortiz.rg@gmail.com>
2020-12-16 17:39:07 -0300

    back end

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -14,7 +13,7 @@ const port = 3001
 
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`listening at http://localhost:${port}`)
 })
 
 // view engine setup
@@ -27,8 +26,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', indexRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require("node-fetch");
+
+var cache = {}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/search', (req,res) => {
+  var {query,page} = req.query
+  !page && (page = 1)
+  if(cache[query] && cache[query][page]){
+    console.log('entró al caché')
+    return res.json(cache[query][page])
+  }
+  var offset = (page - 1) * 30
+  fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${query}&limit=30&offset=${offset}`)
+  .then((res) => res.json())
+  .then((json) => {
+    console.log(json.paging)
+    !cache[query] && (cache[query] = {})
+    cache[query][page] = json.results
+    res.json(json.results)})
+})
+
 module.exports = router;
+
